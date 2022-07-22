e74370820f9e1ef43936bacf6bd1eae85be40f0e
Hongyuan Zhang <zhan6305@umn.edu>
2022-07-22 00:06:33 -0700

    update

--- user/tutorials/python/densityplot.py
+++ user/tutorials/python/densityplot.py
@@ -7,14 +7,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from VLE import DoubleVector as vec
+import os
 
 import sys
 
 #from user.run.Python.TP_diagram import P
 print(sys.path)
+ofpath = os.getenv('FOAM_DIR')
 
-mix = VLE.solver_new(
-    "/home/zhy/new_drive/OpenFOAM-6/user/etc/caseDicts/VLEcase/")
+mix = VLE.solver_new(ofpath+"/user/etc/caseDicts/VLEcase/")
 
 mix.reset_specie(["CO2","H2O","CH4" ,"O2"])
 mix.setX([0.9, 0.01,0.045,0.045])
@@ -41,37 +42,37 @@ vf4=[]
 mix.setTPn_flag(VLE.TPN_TPD_Tud)
 
 for it in np.arange(100,600,1):
-    print(it)
+    #print(it)
     mix.setT(float(it))
     T.append(it)
     mix.setP(5e5)
     vf0.append(mix.vaporfra())
     cp0.append(mix.Cp())
     rho0.append(mix.rho())
-    print(5e5)
+    #print(5e5)
     
     mix.setP(10e5)
 
     vf1.append(mix.vaporfra())
     cp1.append(mix.Cp())
     rho1.append(mix.rho())
-    print(10e5)
+    #print(10e5)
     mix.setP(50e5)
 
     vf2.append(mix.vaporfra())
     cp2.append(mix.Cp())
     rho2.append(mix.rho())
-    print(50e5)
+    #print(50e5)
     mix.setP(100e5)
 
     vf3.append(mix.vaporfra())
     cp3.append(mix.Cp())
     rho3.append(mix.rho())
-    print(100e5)
+    #print(100e5)
     mix.setP(200e5)
     vf4.append(mix.vaporfra())
     rho4.append(mix.rho())
-    print(200e5)
+    #print(200e5)
 fig, ax1 = plt.subplots()
 ax1.plot(T,rho0)
 ax1.plot(T,rho1)
@@ -91,7 +92,7 @@ ax2.plot(T,vf2,'--')
 plt.ylim(0,2)
 #plt.legend(["old","new"],frameon=False,labelspacing=0,fontsize=24,bbox_to_anchor=(0.77, 0.75),loc='center',handlelength=1.2)
 pair = [(T[i],vf1[i]) for i in range(len(T))]
-print(pair)
+#print(pair)
 plt.savefig("vfdbug.png", dpi=300,bbox_inches='tight')
 """
 
