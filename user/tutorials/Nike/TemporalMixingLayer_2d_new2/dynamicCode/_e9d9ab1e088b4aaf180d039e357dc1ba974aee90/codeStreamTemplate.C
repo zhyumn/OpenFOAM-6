38cd73aeb288fcd0ecab70cf69ed471b63896a87
Navneeth Srinivasan <srini237@ahl04.agate.msi.umn.edu>
2022-12-05 09:06:59 -0600

    t

---
+++ user/tutorials/Nike/TemporalMixingLayer_2d_new2/dynamicCode/_e9d9ab1e088b4aaf180d039e357dc1ba974aee90/codeStreamTemplate.C
@@ -0,0 +1,91 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+#line 25 "/panfs/jay/groups/25/suo-yang/srini237/OpenFoamTemp/OpenFOAM-6/user/tutorials/Nike/TemporalMixingLayer_2d_new2/0/T.#codeStream"
+#include "fvCFD.H"
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_e9d9ab1e088b4aaf180d039e357dc1ba974aee90
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 39 "/panfs/jay/groups/25/suo-yang/srini237/OpenFoamTemp/OpenFOAM-6/user/tutorials/Nike/TemporalMixingLayer_2d_new2/0/T.#codeStream"
+const IOdictionary & d = static_cast<const IOdictionary&>(dict);
+            const fvMesh& mesh = refCast<const fvMesh>(d.db());
+            const scalar PI = 3.1415926535897932384626;
+
+            double thickness =5.436e-07;//5.901;
+            double Ttop=555;
+            double Tbottom=293.0;
+
+            scalarField T(mesh.nCells());
+            forAll(T, i)
+            {
+                const scalar z = mesh.C()[i][2];
+ 
+                
+                    T[i] =0.5*(Ttop+Tbottom)+0.5*(Ttop-Tbottom)*erf(sqrt(PI)*z/thickness);
+                
+            }
+            T.writeEntry( "", os);
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
